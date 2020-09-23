1c290f80f9ff0fdfb3e044713464bcd5bb46626b
iagosergiop93 <iagosergiop93@gmail.com>
2020-09-23 13:13:35 -0400

    First commit

---
+++ src/test/java/com/flashcard/configserver/FlashCardConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcard.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
